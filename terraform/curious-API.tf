fbbfc4fc1641e8620053346ff0a33a62d930d491
Richard Adams <richard.adams@digital.justice.gov.uk>
2021-07-05 09:10:16 +0100

    Add Bharat Parmar as Collaborator

--- terraform/curious-API.tf
+++ terraform/curious-API.tf
@@ -12,5 +12,15 @@ module "curious-API" {
       added_by     = "Richard Adams <richard.adams@digital.justice.gov.uk>"
       review_after = "2021-12-26"
     },
+    {
+      github_user  = "bharaatt"
+      permission   = "write"
+      name         = "Bharat Parmar"
+      email        = "bharat.parmar@meganexus.com"
+      org          = "MegaNexus"
+      reason       = "MegaNexus are developing an API into Curious for HMPPS Education, Skills, Work and Employability (ESWE) project"
+      added_by     = "Richard Adams <richard.adams@digital.justice.gov.uk>"
+      review_after = "2021-12-26"
+    },
   ]
 }
